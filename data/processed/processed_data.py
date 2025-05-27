4c20838b0a112a2223df55381ff5869f35493884
Matheesha-Malshan <emalshan88@gmail.com>
2025-05-27 22:57:01 +0530

    Add data, notebooks, and src directories with initial content

---
+++ data/processed/processed_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df=pd.read_csv(r"C:\Users\HP\OneDrive\Desktop\New folder\customer-churn-prediction-pipline\data\processed\processed_data.csv")
+print(df)
---
+++ data/raw/data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+unproceed_data=pd.read_csv(
+    r"C:\Users\HP\OneDrive\Desktop\New folder\customer-churn-prediction-pipline\data\raw\WA_Fn-UseC_-Telco-Customer-Churn.csv"
+)
