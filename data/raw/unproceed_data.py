19b5f5cdefe83bbca266a04e8af55faa0c2cd8a4
Matheesha-Malshan <emalshan88@gmail.com>
2025-05-28 20:25:23 +0530

    add unproceed_data file

---
+++ data/raw/unproceed_data.py
@@ -0,0 +1,19 @@
+import yaml
+from prefect import task, flow
+import pandas as pd
+
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+@task
+def read_data(data_path: str) -> pd.DataFrame:
+    return pd.read_csv(data_path)
+
+@flow
+def data_flow() -> pd.DataFrame:
+    return read_data(data_path=config["data"]["data_path"])
+
+if __name__ == "__main__":
+    df = data_flow()
+    print(df)
+
