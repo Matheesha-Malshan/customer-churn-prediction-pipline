3dd8372420f9f3236f62072caf715e8c792b7da9
Matheesha-Malshan <emalshan88@gmail.com>
2025-05-28 20:19:12 +0530

    Update processed_data.py for outlier removal logic

--- data/processed/processed_data.py
+++ data/processed/processed_data.py
@@ -1,4 +1,33 @@
+import yaml
+from prefect import task, flow
 import pandas as pd
+from sklearn.ensemble import IsolationForest
 
-df=pd.read_csv(r"C:\Users\HP\OneDrive\Desktop\New folder\customer-churn-prediction-pipline\data\processed\processed_data.csv")
-print(df)
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+@task
+def read_filtered_data(data_path: str) -> pd.DataFrame:
+    return pd.read_csv(data_path)
+
+@task
+def remove_outliers(filtered_data: pd.DataFrame) -> pd.DataFrame:
+    iso_forest = IsolationForest(contamination=0.1, random_state=42)
+    iso_pred = iso_forest.fit_predict(filtered_data)
+    mask = iso_pred == 1
+    cleaned_data = filtered_data[mask]
+    return cleaned_data
+
+@flow
+def clean_data_flow(data_path: str) -> pd.DataFrame:
+    df = read_filtered_data(data_path)
+    cleaned_df = remove_outliers(df)
+    return cleaned_df
+
+if __name__ == "__main__":
+    data_path = config["data"]["filltered_data"]
+    cleaned = clean_data_flow(data_path)
+    print(cleaned)
+
+
+
